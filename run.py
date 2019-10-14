2876764969b0901fd6fb6ee616ab80275f960dab
ZakahrovDenis <dr.interned@gmail.com>
2019-10-15 00:29:19 +0300

    File saves now

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request, url_for, redirect, flash
-import os
+import os, datetime
 import io, base64
+
+from settings.paths import DIR, UPLOAD_DIR
 
 app = Flask(__name__)
 
-def getpictures(files):
-    output = io.BytesIO()
-    files.save(output)
+
+
+def getpictures(file_path):
+    output = io.BytesIO(open(file_path, "rb").read())
     pictures = []
-    input_pic = base64.b64encode(output.getvalue()).decode()
-    pictures.append(input_pic)
-    DIR = os.path.join(os.getcwd(), 'templates/media')
+    pictures.append(base64.b64encode(output.getvalue()).decode())
     for fl in os.listdir(DIR):
         output = io.BytesIO(open(os.path.join(DIR, fl), "rb").read())
         pictures.append(base64.b64encode(output.getvalue()).decode())
@@ -19,15 +20,14 @@ def getpictures(files):
 @app.route('/', methods=["GET",'POST'])
 def hello_world():
     if request.method=="POST":
-        if 'file' not in request.files:
-            flash('No file part')
+        if 'file' not in request.files or request.files['file'].filename == "":
             return redirect(request.url)
         file = request.files["file"]
-        pictures = getpictures(file)
-        try:
-            return render_template("result.html", pictures = pictures)
-        except:
-            return redirect(request.url)
+        filename = str(hash(file.filename + str(datetime.datetime.now()))) + "." + file.filename.split(".")[-1]
+        file.save(os.path.join(UPLOAD_DIR, filename))
+        pictures = getpictures(os.path.join(UPLOAD_DIR, filename))
+        os.remove(os.path.join(UPLOAD_DIR, filename))
+        return render_template("result.html", pictures = pictures)            
     return render_template('main.html')
 
 if __name__ == '__main__':
---
+++ settings/paths.py
@@ -0,0 +1,5 @@
+import os
+
+DIR = os.path.join(os.getcwd(), 'templates/media/')
+UPLOAD_DIR = os.path.join(os.getcwd(), 'upload/')
+
