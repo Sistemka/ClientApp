0ec6e8aba700cecb84a4d0cdffdf574993ec7e0e
unknown <dr.interned@gmail.com>
2019-10-14 21:48:49 +0300

    pep

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
-import os, datetime
-import io, base64
+import os
+import datetime
+import io
+import base64
+
+from flask import Flask, render_template, request, redirect
 
 from settings.paths import DIR, UPLOAD_DIR
 
 app = Flask(__name__)
-
 
 
 def getpictures(file_path):
@@ -17,18 +19,21 @@ def getpictures(file_path):
         pictures.append(base64.b64encode(output.getvalue()).decode())
     return pictures
 
-@app.route('/', methods=["GET",'POST'])
+
+@app.route('/', methods=["GET", 'POST'])
 def hello_world():
-    if request.method=="POST":
+    if request.method == "POST":
         if 'file' not in request.files or request.files['file'].filename == "":
             return redirect(request.url)
         file = request.files["file"]
-        filename = str(hash(file.filename + str(datetime.datetime.now()))) + "." + file.filename.split(".")[-1]
+        filename = str(hash(file.filename + str(datetime.datetime.now()))
+                       ) + "." + file.filename.split(".")[-1]
         file.save(os.path.join(UPLOAD_DIR, filename))
         pictures = getpictures(os.path.join(UPLOAD_DIR, filename))
         os.remove(os.path.join(UPLOAD_DIR, filename))
-        return render_template("result.html", pictures = pictures)            
+        return render_template("result.html", pictures=pictures)
     return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run()
