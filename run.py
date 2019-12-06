2bd70d525c21daeec5bb4b3a534ed6f32a02198e
unknown <dr.interned@gmail.com>
2019-12-06 04:18:58 +0300

    top front 2.0 + new search added

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ import datetime
 import io
 import base64
 
-from flask import Flask, render_template, request, redirect
-from sistemka.services import SearchEngine
+from flask import Flask, render_template, request, redirect, abort, Response
+from sistemka.services import SearchEngine, ImageManager
 
-from settings.paths import UPLOAD_DIR
+from settings.paths import UPLOAD_DIR, FILES_DIR
 
 app = Flask(__name__)
 
@@ -16,11 +16,12 @@ def getpictures(pictures_paths):
     for fl in pictures_paths:
         output = io.BytesIO(open(fl, "rb").read())
         pictures.append(base64.b64encode(output.getvalue()).decode())
+        os.remove(fl)
     return pictures
 
 
 SE = SearchEngine()
-
+image_manager = ImageManager()
 
 @app.route('/', methods=["GET", 'POST'])
 def hello_world():
@@ -32,14 +33,30 @@ def hello_world():
                        ) + "." + file.filename.split(".")[-1]
         upload_file_path = os.path.join(UPLOAD_DIR, filename)
         file.save(upload_file_path)  # this thing saves file, so you can use it
-        # this has to be changed to neural method
-        file_paths = SE.predict(
-            image_path=upload_file_path, image_name='upload_file')
-        file_paths = [fl.get('url') for fl in file_paths]
-        pictures = getpictures(file_paths)
+        urls = SE.search(
+            image_path=upload_file_path)
+        for url in urls:
+            image_manager.download_image(
+                url=url,
+                path_to_download=os.path.join(FILES_DIR, url)
+            )
+        uploaded_files = [os.path.join(FILES_DIR, url) for url in urls]
+        pictures = getpictures(uploaded_files)
         os.remove(os.path.join(UPLOAD_DIR, filename))
         return render_template("main.html", pictures=pictures)
     return render_template('main.html')
+
+@app.route('/set', methods=["POST"])
+def set_pic():
+    if request.method == "POST":
+        if 'file' not in request.files or request.files['file'].filename == "":
+            return abort(405)
+        file = request.files["file"]
+        output = io.BytesIO(file.read())
+        picture = base64.b64encode(output.getvalue()).decode()
+        return picture
+    else:
+        return abort(405)
 
 
 if __name__ == '__main__':
--- settings/paths.py
+++ settings/paths.py
@@ -2,3 +2,5 @@ import os
 
 DIR = os.path.join(os.getcwd(), 'templates/media/')
 UPLOAD_DIR = os.path.join(os.getcwd(), 'upload')
+FILES_DIR = os.path.join(os.getcwd(), 'files')
+
